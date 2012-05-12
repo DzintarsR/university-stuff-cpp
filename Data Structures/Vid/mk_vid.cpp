4e26e7ff9d05e85e6db7d86f3ee240b4e5da6bb1
Karlis Babris <karlisba@gmail.com>
2012-05-12 23:57:09 +0300

    Reads ans lists data into packets

--- Data Structures/Vid/mk_vid.cpp
+++ Data Structures/Vid/mk_vid.cpp
@@ -7,13 +7,68 @@ struct Packet {
     int docId;
     Packet *next;
     Packet() {next=NULL;}
+    Packet(int n) {next=NULL; docId=n;}
+    Packet(int n, Packet *np) {next=np; docId=n;}
 };
 
 // Paketes uztur pēc rindas
 struct PacketRow {
     int docs;
-    Packet *firstPacket;
+    Packet *firstDoc;
     PacketRow *next;
+    PacketRow() {next=NULL;}
+    PacketRow(int d, Packet *p) {docs=d; next=NULL; firstDoc=p; }
+};
+
+class packetHandler {
+    private:
+        PacketRow *firstP, *newestP;
+        Packet *currentP;
+        int docsInCurrentP;
+    public:
+        packetHandler() {
+            newestP = firstP = NULL;
+            currentP = NULL;
+            docsInCurrentP=0;
+        };
+        ~packetHandler() {
+            Packet *a, *ca;
+            PacketRow *p=firstP, *cp;
+            while (p != NULL) {
+                a = p->firstDoc;
+                while (a!=NULL) {
+                    ca = a; a=a->next;
+                    delete ca;
+                }
+                cp = p; p=p->next;
+                delete cp;
+            }
+        };
+        void createList(int pId) {
+            if (currentP == NULL) {
+                currentP = new Packet(pId);
+            } else {
+                Packet *cp;
+                cp = new Packet(pId, currentP);
+                currentP = cp;
+            }
+            docsInCurrentP++;
+        };
+        void addDoc(int pId) {
+            if (pId != 0) createList(pId);
+            else {
+                if (firstP == NULL) {
+                    newestP = firstP = new PacketRow(docsInCurrentP, currentP);
+                } else {
+                    PacketRow *pc;
+                    pc = new PacketRow(docsInCurrentP, currentP);
+                    newestP->next = pc;
+                    newestP = pc;
+                }
+                docsInCurrentP=0;
+                currentP=NULL;
+            }
+        };
 };
 
 int main() {
@@ -21,4 +76,24 @@ int main() {
     FILE* outputFile;
     inputFile = fopen("mk_vid.in", "r");
 	outputFile = fopen("mk_vid.out", "w+");
+
+	packetHandler *pack = new packetHandler;
+
+    char command[10];
+	int check, doc;
+
+	while (inputFile) {
+	    check = fscanf(inputFile, "%s", command);
+        if (check==EOF) break;
+
+        if (command[0] == 'P') {
+            while (inputFile) {
+                fscanf(inputFile, "%d", &doc);
+                pack->addDoc(doc);
+                if (doc == 0) break;
+            }
+        }
+	}
+
+	return 0;
 }
