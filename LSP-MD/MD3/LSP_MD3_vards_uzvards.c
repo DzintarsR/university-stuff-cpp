790e5adc25e4f780cba91ba14fd482aa2dcaf97f
karlisba <karlisba@gmail.com>
2013-10-20 13:28:36 +0300

    LSP MD3 1st v. finished 60%

---
+++ LSP-MD/MD3/LSP_MD3_vards_uzvards.c
@@ -0,0 +1,155 @@
+#include "stdio.h"
+#include "string.h"
+#include "dirent.h"
+#include "stdlib.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#define NAME_SIZE 255
+#define DIR_SIZE 255
+#define BASE_DIR "."
+
+struct File {
+    char name[NAME_SIZE];
+    char dir[DIR_SIZE];
+    long long size;
+
+    struct File *match;
+    struct File *next;
+};
+
+typedef struct File File_t;
+
+int name_length(char *name) {
+    int i=0;
+    while (1) {
+        if (name[i] == '\0') {
+            return  i;
+        }
+        i++;
+    }
+}
+
+void append_file(struct File *first_file, struct File *new_file, int type) {
+    while (first_file != NULL) {
+        int matched_default = 0;
+        int matched_time = 0;
+        int matched_md5 = 0;
+
+        // By name, size
+        if (type == 1) {
+            matched_time = 1;
+            matched_md5 = 1;
+        }
+
+        // By name, size, date
+        if (type == 2) {
+            matched_md5 = 1;
+        }
+
+        // By md5
+        if (type == 3) {
+            matched_default = 1;
+            matched_time = 1;
+        }
+
+        if (type == 1) {
+            if (strncmp(first_file->name, new_file->name, name_length(new_file->name)) == 0 && first_file->size == new_file->size) {
+                matched_default = 1;
+            }
+        }
+
+        if (matched_default && matched_time && matched_md5) {
+            if (first_file->match == NULL) {
+                first_file->match = new_file;
+                break;
+            } else {
+                File_t *fm = first_file->match;
+                while (fm != NULL) {
+                    if (fm->match == NULL) {
+                        fm->match = new_file;
+                        break;
+                    }
+                    fm = fm->match;
+                }
+                break;
+            }
+        }
+
+        if (first_file->next == NULL) {
+            first_file->next = new_file;
+            break;
+        }
+
+        first_file = first_file->next;
+    }
+}
+
+void match_dir_file(DIR *pDir, char *dir, struct File *file) {
+    struct dirent *pDirent;
+
+    if (pDir == NULL) {
+        return;
+    }
+
+    while((pDirent = readdir(pDir)) != NULL) {
+        struct stat p_statbuf;
+
+        char filename[500];
+        strcpy(filename, dir);
+        strcat(filename, "/");
+        strcat(filename, pDirent->d_name);
+
+        lstat(filename, &p_statbuf);
+
+        // Ignore soft links
+        if (S_ISLNK(p_statbuf.st_mode)) {
+            continue;
+        }
+
+        if (pDirent->d_type != DT_DIR) {
+            File_t *f = malloc(sizeof(struct File));
+            strcpy(f->name, pDirent->d_name);
+            strcpy(f->dir, dir);
+            f->size = p_statbuf.st_size;
+            //printf("%s\n", ctime(&p_statbuf.st_ctime));
+            append_file(file, f, 1);
+        } else {
+            if (strncmp(pDirent->d_name, "..", 2) != 0 && strncmp(pDirent->d_name, ".", 1) != 0) {
+                DIR *tDir;
+                tDir = opendir(filename);
+                match_dir_file(tDir, filename, file);
+                closedir(tDir);
+            }
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    struct dirent *pDirent;
+    DIR *pDir;
+    File_t *first = malloc(sizeof(struct File));
+
+    pDir = opendir(BASE_DIR);
+
+    match_dir_file(pDir, BASE_DIR, first);
+
+    while (first != NULL) {
+        if (first->match != NULL) {
+            printf("=== %lld %s\n", first->size, first->name);
+            File_t *f = first;
+            while (f != NULL) {
+                printf("%s/%s\n", f->dir, f->name);
+                f = f->match;
+            }
+            printf("...\n");
+        }
+
+        first = first->next;
+    }
+
+    closedir(pDir);
+
+    return 0;
+}
