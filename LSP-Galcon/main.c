a813fe04c99ea3c4151d654c5230392f2e23c6da
karlisba <karlisba@gmail.com>
2014-01-18 19:55:08 +0200

    Newest version of Galcon game server

--- LSP-Galcon/main.c
+++ LSP-Galcon/main.c
@@ -2,11 +2,15 @@
 #include "stdlib.h"
 #include "string.h"
 #include "math.h"
+#include "time.h"
+
+#define REFRESH_SECONDS 1
+#define INITIAL_SHIP_PERCENTAGE 10
 
 struct User {
     int id;
     char username[50];
-    char ip[15];
+    char ip[21];
 
     struct User *next;
 };
@@ -15,26 +19,29 @@ typedef struct User User_t;
 
 struct Planet {
     int id;
-    int user_id;
     int x;
     int y;
     int capacity;
     int ships;
 
+    struct User *user;
     struct Planet *next;
 };
 
 typedef struct Planet Planet_t;
 
 struct Attack {
-    int planet_from;
-    int planet_to;
     int ships;
-    int attacker_user_id;
-    int defender_user_id;
     int time_left;
 
+    struct Planet *planet_from;
+    struct Planet *planet_to;
+
+    struct User *attacker_user;
+    struct User *defender_user;
+
     struct Attack *next;
+    struct Attack *prev;
 };
 
 typedef struct Attack Attack_t;
@@ -51,18 +58,10 @@ int max_planet_capacity = 0;
 int min_planet_capacity = 0;
 
 Planet_t *get_planet_by_id(int planet_id) {
-    Planet_t *f = first_planet;
-    while (f != NULL) {
-        if (f->id == planet_id) return f;
-    }
-
-    return NULL;
-}
-
-User_t *get_user_by_id(int user_id) {
-    User_t *f = first_user;
-    while (f != NULL) {
-        if (f->id == user_id) return f;
+    Planet_t *planet = first_planet;
+    while (planet != NULL) {
+        if (planet->id == planet_id) return planet;
+        planet = planet->next;
     }
 
     return NULL;
@@ -76,18 +75,42 @@ User_t *get_user_by_ip(char *user_ip) {
         }
         user = user->next;
     }
+
     return user;
 }
 
+Planet_t *get_free_planet() {
+    Planet_t *planet = first_planet;
+    Planet_t *hold = NULL;
+
+    while (planet != NULL) {
+        if (planet->user == NULL) {
+            if (hold == NULL) {
+                hold = planet;
+            } else {
+                if (planet->capacity > hold->capacity) {
+                    hold = planet;
+                }
+            }
+        }
+
+        planet = planet->next;
+    }
+
+    return hold;
+}
+
 int get_attack_count(int user_id) {
-    Attack_t *p = first_attack;
+    Attack_t *attack = first_attack;
     int i = 0;
-    while (p != NULL) {
-        if (p->defender_user_id == user_id) {
+
+    while (attack != NULL) {
+        if (attack->defender_user->id == user_id) {
             i++;
         }
-        p = p->next;
+        attack = attack->next;
     }
+
     return i;
 }
 
@@ -97,34 +120,48 @@ int get_travel_time(int from_x, int from_y, int to_x, int to_y) {
     return distance / game_speed;
 }
 
+int get_planet_capacity() {
+    return (rand() % (max_planet_capacity - min_planet_capacity)) + min_planet_capacity;
+}
+
+int get_new_ships(int planet_capacity) {
+    return planet_capacity / min_planet_capacity;
+}
+
 char *get_map_data() {
-    Planet_t *p = first_planet;
+    Planet_t *planet = first_planet;
     char buffer[255];
     char *command = (char *) malloc(sizeof(char));
     sprintf(command, "M %d", generated_planets);
 
-    while (p != NULL) {
+    while (planet != NULL) {
         sprintf(buffer, " %d_%d_%d_%d_%d_%d", 
-            p->id, p->x, p->y, p->user_id, p->capacity, p->ships);
+            planet->id, 
+            planet->x, 
+            planet->y, 
+            planet->user == NULL ? 0 : planet->user->id, 
+            planet->capacity, 
+            planet->ships
+        );
         strcat(command, buffer);
 
-        p = p->next;
+        planet = planet->next;
     }
 
     return command;
 }
 
 char *get_user_data() {
-    User_t *u = first_user;
+    User_t *user = first_user;
     char buffer[100];
     char *command = (char *) malloc(sizeof(char));
     sprintf(command, "U %d", registered_players);
 
-    while (u != NULL) {
-        sprintf(buffer, " %d_%s", u->id, u->username);
+    while (user != NULL) {
+        sprintf(buffer, " %d_%s", user->id, user->username);
         strcat(command, buffer);
 
-        u = u->next;
+        user = user->next;
     }
 
     return command;
@@ -133,23 +170,29 @@ char *get_user_data() {
 char *get_attack_data(char *user_ip) {
     User_t *user = get_user_by_ip(user_ip);
 
+    /* Validate user existence */
     if (user == NULL) {
         return "A 0";
     }
 
-    Attack_t *a = first_attack;
+    Attack_t *attack = first_attack;
     char buffer[100];
     char *command = (char *) malloc(sizeof(char));
     sprintf(command, "A %d", get_attack_count(user->id));
 
-    while (a != NULL) {
-        if (a->defender_user_id == user->id) {
+    while (attack != NULL) {
+        if (attack->defender_user->id == user->id) {
             sprintf(buffer, " %d_%d_%d_%d_%d", 
-                a->planet_to, a->ships, a->planet_from, a->attacker_user_id, a->time_left);
+                attack->planet_to->id, 
+                attack->ships, 
+                attack->planet_from->id, 
+                attack->attacker_user->id, 
+                attack->time_left
+            );
             strcat(command, buffer);
         }
 
-        a = a->next;
+        attack = attack->next;
     }
 
     return command;
@@ -160,6 +203,13 @@ char *join_game(char *username, char *user_ip) {
         return "J 0 0";
     }
 
+    /* Check existence for free planet */
+    Planet_t *planet = get_free_planet();
+    if (planet == NULL) {
+        return "J 0 0";
+    }
+
+    /* Append new user to list */
     User_t *user = first_user;
     for (;;) {
         if (first_user == NULL) {
@@ -176,34 +226,50 @@ char *join_game(char *username, char *user_ip) {
 
     registered_players++;
 
+    /* Set planet for user  */
+    planet->user = user;
+
+    /* Set user data */
     user->id = registered_players;
     user->next = NULL;
     strcpy(user->username, username);
-    strcpy(user->ip, user_ip);
+    strcpy(user->ip, user_ip);    
 
+    /* Create output string */
     char *command = (char *) malloc(sizeof(char));
     sprintf(command, "J %d %d", user->id, seconds_till_start);
     
     return command;
 }
 
-char *create_attack(int planet_from_id, int planet_to_id, int ships, char *user_ip) {
+char *create_attack(int planet_from_id, int planet_to_id, int ships, char *attacker_ip) {
     Planet_t *planet_from = get_planet_by_id(planet_from_id);
     Planet_t *planet_to = get_planet_by_id(planet_to_id);
-    User_t *attacker_user = get_user_by_ip(user_ip);
-    User_t *defender_user = get_user_by_id(planet_to->user_id);
+    
+    User_t *attacker_user = get_user_by_ip(attacker_ip);
+    User_t *defender_user = planet_to->user;
 
-    if (planet_from->user_id != attacker_user->id) {
+    if (planet_from == NULL || planet_to == NULL) {
+        return "S 0 0";
+    }
+
+    if (attacker_user == NULL) {
+        return "S 0 0";
+    }
+
+    if (planet_from->user != attacker_user) {
         return "S 0 0";
     }
 
     Attack_t *attack = first_attack;
+    Attack_t *prev = NULL;
     for (;;) {
         if (first_attack == NULL) {
             attack = first_attack = malloc(sizeof(struct Attack));
             break;
         }        
         if (attack->next == NULL) {
+            prev = attack;
             attack = attack->next = malloc(sizeof(struct Attack));
             break;
         }
@@ -221,12 +287,13 @@ char *create_attack(int planet_from_id, int planet_to_id, int ships, char *user_
 
     int time = get_travel_time(planet_from->x, planet_from->y, planet_to->x, planet_to->y);
 
-    attack->planet_from = planet_from->id;
-    attack->planet_to = planet_to->id;
+    attack->planet_from = planet_from;
+    attack->planet_to = planet_to;
     attack->ships = ships;
-    attack->attacker_user_id = attacker_user->id;
-    attack->defender_user_id = defender_user->id;
+    attack->attacker_user = attacker_user;
+    attack->defender_user = defender_user;
     attack->time_left = time;
+    attack->prev = prev;
 
     char *command = (char *) malloc(sizeof(char));
     sprintf(command, "S %d %d", ships, time);
@@ -234,61 +301,193 @@ char *create_attack(int planet_from_id, int planet_to_id, int ships, char *user_
     return command;
 }
 
+void set_planet_data(Planet_t *planet, int x, int y, int id) {
+    planet->id = id;
+    planet->x = x;
+    planet->y = y;
+    planet->capacity = get_planet_capacity();
+    planet->ships = planet->capacity * (INITIAL_SHIP_PERCENTAGE / (double)100);
+    planet->next = NULL;
+    planet->user = NULL;
+}
+
+void generate_planets(FILE *planets) {
+    int planet_id = 1;
+    int x, y;
+    Planet_t *planet;
+
+    while (fscanf(planets, "%d %d", &x, &y) != EOF) {
+        if (first_planet == NULL) {
+            planet = first_planet = malloc(sizeof(struct Planet));
+            set_planet_data(planet, x, y, planet_id);
+        } else {
+            planet = planet->next = malloc(sizeof(struct Planet));
+            set_planet_data(planet, x, y, planet_id);
+        }
+        planet_id++;
+    }
+
+    generated_planets = planet_id - 1;
+
+    printf("--- GENERATED PLANETS ---\n");
+    planet = first_planet;
+    while (planet != NULL) {
+        printf("X: %d\t Y: %d\t CAPACITY: %d\t SHIPS: %d\n", 
+            planet->x, planet->y, planet->capacity, planet->ships);
+
+        planet = planet->next;
+    }
+    printf("--- GENERATED PLANETS ---\n\n");
+}
+
+void calculate_attacks() {
+    Attack_t *attack = first_attack;
+
+    while (attack != NULL) {
+        attack->time_left -= 1;
+        
+        if (attack->time_left == 0) {
+            /* Handle planet takeover */
+            Planet_t *planet = attack->planet_to;
+
+            if ((planet->ships - attack->ships) <= 0) {
+                planet->user = attack->attacker_user;
+                planet->ships = attack->ships - planet->ships;
+            } else {
+                planet->ships -= attack->ships;
+            }
+
+            /* Remove attack from list */
+            if (attack == first_attack) {
+                first_attack = attack->next;
+                if (first_attack != NULL) {
+                    first_attack->prev = NULL;
+                }
+            } else {
+                attack->prev->next = attack->next;
+                if (attack->next != NULL) {
+                    attack->next->prev = attack->prev;
+                }
+            }
+
+            free(attack);
+        }
+        
+        attack = attack->next;
+    } 
+}
+
+void calculate_ships() {
+    int new_ships, user_id;
+    Planet_t *planet = first_planet;
+
+    printf("--- GENERATED SHIPS ---\n");
+    while (planet != NULL) {
+        if (planet->ships != planet->capacity) {
+            new_ships = get_new_ships(planet->capacity);
+        
+            if ((planet->ships + new_ships) >= planet->capacity) {
+                planet->ships = planet->capacity;
+            } else {
+                planet->ships += new_ships;
+            }
+        }
+
+        user_id = (planet->user == NULL) ? 0 : planet->user->id;
+        printf("PLANET: %d\t CAPACITY: %d\t SHIPS: %d\t USER: %d\n", 
+            planet->id, planet->capacity, planet->ships, user_id);
+
+        planet = planet->next;
+    }
+    printf("--- GENERATED SHIPS ---\n\n");
+}
+
 int main (int argc, char *argv[]) {
-    max_players = 10;
-    seconds_till_start = 30;
+    FILE *file[2];
+
+    char buffer[20];
+    int value;
+    int i;
+
+    /* For more random numbers */
+    srand(time(NULL));
+
+    if (argc < 3) {
+        printf("Specify program with input: ./main config_file planet_file\n");
+        return 0;
+    }
+
+    for (i=0; i<=1; i++) {
+        if ((file[i] = fopen(argv[i+1], "r")) == NULL) {
+            printf("Can't open config file: %s\n", argv[i+1]);
+            return 0;
+        }
+    }
+
+    while (fscanf(file[0], "%s %d", buffer, &value) != EOF) {
+        if (strcmp(buffer, "PLAYERS") == 0) {
+            max_players = value;
+        }
+
+        if (strcmp(buffer, "SPEED") == 0) {
+            game_speed = value;
+        }
+
+        if (strcmp(buffer, "START_TIME") == 0) {
+            seconds_till_start = value;
+        }
+
+        if (strcmp(buffer, "MIN_CAPACITY") == 0) {
+            min_planet_capacity = value;
+        }
+
+        if (strcmp(buffer, "MAX_CAPACITY") == 0) {
+            max_planet_capacity = value;
+        }
+    }
+
+    /* Generate planets */
+    generate_planets(file[1]);
+
+    double time_counter = 0;
+    int game_time = 0;
+    clock_t this_time = clock();
+    clock_t last_time = this_time;
+
+    for (;;) {
+        this_time = clock();
+        time_counter += (double)(this_time - last_time);
+        last_time = this_time;
+
+        if(time_counter > (double)(REFRESH_SECONDS * CLOCKS_PER_SEC)) {
+            /* TODO: Check and call J, M, S, U, A commands */
+
+            if (seconds_till_start > 0) {
+                printf("Starting after %d seconds\n", seconds_till_start);
+                seconds_till_start--;
+            } else {
+                printf("GAME TIME IN SECONDS: %d\n\n", game_time);
+
+                /* Recalculate ship movements and planet status */
+                calculate_attacks();
+
+                /* Recalculate ships on planets */
+                calculate_ships();
+
+                game_time++;
+            }
+            time_counter -= (double)(REFRESH_SECONDS * CLOCKS_PER_SEC);
+        }
+    }
+
     /*
-    first_planet = malloc(sizeof(struct Planet));
-    first_planet->id = 1;
-    first_planet->user_id = 1;
-    first_planet->x = 10;
-    first_planet->y = 15;
-    first_planet->capacity = 100;
-    first_planet->ships = 30;
-    
-    Planet_t *n = first_planet->next = malloc(sizeof(struct Planet));
-    n->id = 2;
-    n->user_id = 2;
-    n->x = 20;
-    n->y = 30;
-    n->capacity = 70;
-    n->ships = 50;
-    n->next = NULL;
+    Command samples:
+    J: join_game("karlis", "0.0.0.0:58265");
+    M: get_map_data();
+    S: create_attack(1, 2, 30, "0.0.0.0:58265");
+    U: get_user_data();
+    A: get_attack_data("0.0.0.0:58265");
     */
-
-    //printf("%s\n", get_map_data());
-
-    /*
-    first_user = malloc(sizeof(struct User));
-    first_user->id = 1;
-    strcpy(first_user->ip, "02.125.156.14");
-    strcpy(first_user->username, "karlis");
-    
-    User_t *u = first_user->next = malloc(sizeof(struct User));
-    u->id = 2;
-    strcpy(u->ip, "159.132.154.25");
-    strcpy(u->username, "janis");
-    u->next = NULL;
-    */
-
-    //printf("%s\n", get_user_data());
-    
-    /*
-    first_attack = malloc(sizeof(struct Attack));
-    first_attack->planet_from = 1;
-    first_attack->planet_to = 2;
-    first_attack->ships = 10;
-    first_attack->attacker_user_id = 1;
-    first_attack->defender_user_id = 2;
-    first_attack->time_left = 30;
-    first_attack->next = NULL;
-
-    printf("%s\n", get_attack_data("159.132.154.25"));
-    */
-
-    printf("%s\n", join_game("karlis", "02.125.156.14"));
-    printf("%s\n", join_game("janis", "159.132.154.25"));
-    printf("%s\n", join_game("peteris", "159.132.154.25"));
     
     return 0;
 }
